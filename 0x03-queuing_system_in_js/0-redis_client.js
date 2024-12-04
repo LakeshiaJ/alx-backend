2961a0953354b54879b9a831608247e000888634
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-12-04 14:45:36 +0000

    0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,5 +1,26 @@
-import redis from "redis";
-const client = redis.createClient();
-client.on('error', err => { 
-    console.log(`Redis client not connected to the server: ${err}`)
+
+import redis from 'redis';
+
+// Create a Redis Client
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+// Attempt to connect to the Redis server
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Handle errors during connection
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+// Close the Redis connection when the script exists
+process.on('SIGINT', () => {
+  client.quit(() => {
+    console.log('Redis client disconnected');
+    process.exit();
+  });
 });
